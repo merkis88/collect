6b3bf9d1298688f97957dcf3597de7511a8dc6a2
merkis88 <axuenno88@gmail.com>
2025-05-14 23:11:19 +0700

    add collect

---
+++ src/helpers.php
@@ -0,0 +1,19 @@
+<?php
+namespace Collect;
+
+function collection(array $array = []): Collect
+{
+    return new Collect($array);
+}
+
+namespace Collect;
+
+require_once 'vendor/autoload.php';
+
+$records =
+    [
+        'id' => 2135,
+        'first_name' => 'John',
+        'last_name' => 'Doe',
+    ];
+var_dump(collection($records)->toArray());
